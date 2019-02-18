89efa6f5543351778d89ffe79b8da8a91b9062d2
Jaouad93EL <elhorm_j@etna-alternance.net>
2019-02-18 11:23:11 +0100

    ertyi*

--- read.c
+++ read.c
@@ -1,23 +1,34 @@
 #include "my.h"
 
-int main(int argc, char** argv) {
+int sizeF(char *str)
+{
+  int n = 0;
+  char c;
+  FILE *file;
+  if ((file = fopen(str, "r")) != NULL) {
+      while((c =fgetc(file)) != EOF){
+        if(c == '\n')
+          ++n;
+      }
+    } else
+      return (-1);    
+  fclose(file);
+  return (n);
+}
 
-  char c[1000];
+
+int main(int argc, char** argv) 
+{
+
+  //char *str = malloc(1000);
   FILE *file;
 
   for (int i = 1; i < argc; ++i) {
-    if ((file = fopen(argv[i], "r")) == NULL)
-    {
-      printf("Error! opening file");
-      exit(1);
-    }
-
-    fscanf(file,"%[^\n]", c);
-
-    printf("%s\n", c);
+    if ((file = fopen(argv[i], "r")) != NULL) {
+         printf("%d\n", sizeF(argv[i]));
+    } else
+      return (-1);    
   }
-
   fclose(file);
-
   return 0;
 }
