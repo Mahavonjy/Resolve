ee039339d620b4b0546844f34d3ae7b7abf2b227
Mahavonjy <mahavonjy.cynthion@gmail.com>
2019-02-18 10:27:42 +0100

    add h

---
+++ my.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef MY_H_
+# define MY_H_
+
+int my_putstr(char *str);
+int edit(char *str);
+void my_putchar(char c);
+int main(int argc, char* argv[]);
+
+#endif /* MY_H_ */
